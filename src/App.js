80b430b0ed1e0469103e53078c3f8b60137d4b45
Heitor da Costa <heitor2000costa@gmail.com>
2022-04-26 19:08:09 -0300

    requisito 3

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
 import { Route, BrowserRouter } from 'react-router-dom';
+import Carrinho from './services/pages/Carrinho';
+import Home from './services/pages/Home';
 import './App.css';
-import Home from './services/componente/Home';
+import { Switch } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div>
-          <Route path="/" component={ Home } />
+          <Switch>
+            <Route exact path="/" component={ Home } />
+            <Route path="/carrinho" component={ Carrinho } />
+          </Switch>
+          
         </div>
       </BrowserRouter>
-
     );
   }
 }
