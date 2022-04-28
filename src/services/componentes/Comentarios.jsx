f770c9a68edb187aed8aa5acd91a20437c6f49d2
Dayane Chiozzi <daychiozzi@gmail.com>
2022-04-28 14:52:22 -0300

    requisito 11

---
+++ src/services/componentes/Comentarios.jsx
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+
+export default class Comentarios extends Component {
+  render() {
+    const { productId, avaliation } = this.props;
+    const productComents = avaliation.filter((i) => i.productId === productId);
+    console.log(productId);
+    console.log(avaliation);
+    console.log(productComents);
+    return (
+      <div>
+        Comentarios
+        <section>
+          { productComents.map((e, index) => (
+            <div key={ index }>
+              <p>{e.email}</p>
+              <p>{e.comentario}</p>
+              <input
+                data-testid="1-rating"
+                id="rating1"
+                type="radio"
+                checked={ e.rating1 }
+              />
+
+              <input
+                data-testid="2-rating"
+                id="rating2"
+                type="radio"
+                checked={ e.rating2 }
+              />
+
+              <input
+                data-testid="3-rating"
+                id="rating3"
+                type="radio"
+                checked={ e.rating3 }
+              />
+
+              <input
+                data-testid="4-rating"
+                id="rating4"
+                type="radio"
+                checked={ e.rating4 }
+              />
+
+              <input
+                data-testid="5-rating"
+                id="rating5"
+                type="radio"
+                checked={ e.rating5 }
+              />
+            </div>
+          ))}
+        </section>
+      </div>
+    );
+  }
+}
